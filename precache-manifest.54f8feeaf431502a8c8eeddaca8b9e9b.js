fab04f6311239c18bc6384c48b86e1d5b818537c
Karl Bennett <widemouthfrog1@gmail.com>
2020-06-22 14:55:54 +1200

    Updates

---
+++ precache-manifest.54f8feeaf431502a8c8eeddaca8b9e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb782a67677dc5d964e6283e306e5353",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "40767b91fd69e102203d",
+    "url": "/Portfolio-Website/static/css/main.d1b8ace6.chunk.css"
+  },
+  {
+    "revision": "d0412a2375b3648cf32c",
+    "url": "/Portfolio-Website/static/js/2.ca4817d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-Website/static/js/2.ca4817d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40767b91fd69e102203d",
+    "url": "/Portfolio-Website/static/js/main.f7c06a2c.chunk.js"
+  },
+  {
+    "revision": "35c7624aa1f9b0da3ac6",
+    "url": "/Portfolio-Website/static/js/runtime-main.cd2e2bdc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94474825bac472fc2efb6339ad44dfb1.js"
+  "/Portfolio-Website/precache-manifest.54f8feeaf431502a8c8eeddaca8b9e9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-Website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

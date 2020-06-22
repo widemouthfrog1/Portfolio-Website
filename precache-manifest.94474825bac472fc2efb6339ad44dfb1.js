1f23fc1ada85ecc16c1b10b79e2942ea77d8b3fd
Karl Bennett <widemouthfrog1@gmail.com>
2020-06-22 14:49:18 +1200

    Updates

---
+++ precache-manifest.94474825bac472fc2efb6339ad44dfb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ba17e16a7e666951ab9ed37f6d7487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e9206b04bddd6d6d19c",
+    "url": "/static/css/main.d1b8ace6.chunk.css"
+  },
+  {
+    "revision": "d0412a2375b3648cf32c",
+    "url": "/static/js/2.ca4817d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca4817d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9206b04bddd6d6d19c",
+    "url": "/static/js/main.18779fb8.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  }
+]);

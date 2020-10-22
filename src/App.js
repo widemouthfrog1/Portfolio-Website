f8159ce0b6f6502631398d89cade9a1d83317741
Karl Bennett <widemouthfrog1@gmail.com>
2020-10-22 19:17:56 +1300

    Remove most files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Game from "./gameObjects/Game";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Game />
       </header>
     </div>
   );
